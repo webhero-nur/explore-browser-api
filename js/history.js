709b725fd1104e22949a0fdb910e66f0972b47f8
webhero-nur <nurealom522@gmail.com>
2022-11-02 22:24:48 +0600

    History api to navigate back, forward, go, history length

---
+++ js/history.js
@@ -0,0 +1,10 @@
+// history.back() --> to go back
+// history.forward --> to go forward
+
+// history.go() --> reload current page
+// history.go(-1) --> go 1 step back
+// history.go(1) --> go 1 step forward
+// history.go(-3) --> go 3 step back
+// history.go(3) --> go 3 step forward
+
+// history.length --> check how many histories are available
