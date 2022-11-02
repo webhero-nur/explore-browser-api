8036b7179351f16f5e9170b4cb97a21bcb820219
webhero-nur <nurealom522@gmail.com>
2022-11-02 21:44:59 +0600

    Explore BOM. Browser alert, confirm and prompt with example

---
+++ js/promt.js
@@ -0,0 +1,28 @@
+console.log('promt.js connected');
+// alert('ki khobor dosto');
+const showAlert = () => {
+    const num = Math.random() * 10;
+    console.log(num);
+    if (num < 5) {
+        alert('ki khobor dosto');
+    }
+}
+
+const aksSomething = () => {
+    const decision = confirm('Are you coming to Picnic?');
+    console.log(decision);
+    if (decision === true) {
+        alert('dosto 200tk. bKash kor');
+    }
+    else {
+        console.log('DGM!!! Door e giya mor');
+    }
+}
+
+const getUserInfo = () => {
+    const name = prompt('Tell us your name.');
+    console.log(name);
+    if (!!name) {
+        console.log('Welcome here', name);
+    }
+}
