dcd98da86a89d99a2f36c5a9bb282c926245b5ae
webhero-nur <nurealom522@gmail.com>
2023-01-13 19:59:40 +0600

    restart

--- js/cookies.js
+++ js/cookies.js
@@ -13,3 +13,12 @@ document.getElementById('btn-find').addEventListener('click', function () {
     const value = getCookiesByName(document.getElementById('cookie-inpt').value = '');
     alert(value);
 })
+
+// get cookie value from browser
+document.cookie
+
+// get cookie value as array
+document.cookie.split(': ')
+
+// get cookie value in key-value pair
+document.cookie.split('; ').map(c => console.log(c))
